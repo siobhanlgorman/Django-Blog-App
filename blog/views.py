06a6653bdc3e41e89cc4caf1e76ef8635d527a4e
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-22 14:00:01 +0000

    Wire up urls

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from .models import Post
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.onjects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
 
