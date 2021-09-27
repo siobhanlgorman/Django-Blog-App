eee4cffeca50f76d6a6ef661d4d2a842b61b2e9f
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-27 11:34:58 +0000

    First attempt to write test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from forms import CommentForm
 
 # Create your tests here.
+
+# class TestCommentForm(TestCase):
+#     def test_comment_body_is_required(self):
+#         form = CommentForm({'body': ''})
+#         self.assertFalse(form.is_valid())
+#         self.asssertIn('name', form.errors.keys())
+#         self.assertEqual(form.errors['body'][0], 'This field is required')
