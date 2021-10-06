6f363d6ebb1218ab5eaeaa4c070c7411755ab5b0
siobhanlgorman <siobhanlgorman@gmail.com>
2021-10-06 20:30:05 +0000

    Display success message when comment is submitted

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
 
-        )
+            )
 
     def post(self, request, slug, *args, **kwargs):
 
@@ -45,29 +45,29 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        # comment_form = CommentForm(data=request.POST)
+        comment_form = CommentForm(data=request.POST)
 
-        # if comment_form.is_valid():
-        #     comment_form.instance.email = request.user.email
-        #     comment_form.instance.name = request.user.username
-        #     comment = comment_form.save(commit=False)
-        #     comment.post = post
-        #     comment.save()
-        #     messages.add_message(request, messages.SUCCESS, 'You have successfully added a comment!')
-        # else:
-        #     comment_form = CommentForm()
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'You have successfully added a comment!')
+        else:
+            comment_form = CommentForm()
 
-        #     return render(
-        #         request,
-        #         "post_detail.html",
-        #         {
-        #             "post": post,
-        #             "comments": comments,
-        #             "commented": True,
-        #             "liked": liked,
-        #             "comment_form": CommentForm()
-        #         }
-        #     )
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
 
 class PostLike(View):
